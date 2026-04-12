cac28c55bf2c8af2780cda4e469b494db5d905bc
Ajit Kumar Sahu <sahuajitkumara@gmail.com>
2026-04-12 13:22:03 +0530

    Change Database Connection code and Modifi application.properties

--- src/main/java/com/rms/util/DBConnection.java
+++ src/main/java/com/rms/util/DBConnection.java
@@ -12,27 +12,35 @@ public class DBConnection {
     private static String password;
 
     static {
-        /* Load database properties */
         try {
             Properties props = new Properties();
-            InputStream input = DBConnection.class
-                .getClassLoader()
-                .getResourceAsStream("application.properties");
-
+            InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("application.properties");
+                    
             props.load(input);
 
             url = props.getProperty("db.url");
             username = props.getProperty("db.username");
-            password = props.getProperty("");
+            password = props.getProperty("db.password");
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            System.out.println("✅ DB Properties Loaded");
+
         } catch (Exception e) {
+            System.out.println("❌ DB INIT ERROR");
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(url, username, password);
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            System.out.println("✅ DB Connected Successfully");
+            return con;
+        } catch (Exception e) {
+            System.out.println("❌ DB CONNECTION FAILED");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
